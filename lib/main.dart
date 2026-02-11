1932457c2653cc37752d10ff6835e9af577e68ca
Raihan <raihanwibyanto@gmail.com>
2026-02-11 18:16:14 +0700

    update: main and modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'common/styles.dart';
 import 'data/api/api_service.dart';
+import 'data/db/database_helper.dart';
+import 'data/preferences/preferences_helper.dart';
 import 'provider/restaurant_provider.dart';
+import 'provider/database_provider.dart';
+import 'provider/preferences_provider.dart';
+import 'provider/scheduling_provider.dart';
 import 'ui/restaurant_list_page.dart';
+import 'utils/background_service.dart';
+import 'utils/notification_helper.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper notificationHelper = NotificationHelper();
+  final BackgroundService service = BackgroundService();
+
+  service.initializeIsolate();
+
+  try {
+    await Workmanager().initialize(
+      BackgroundService.callbackDispatcher,
+      isInDebugMode: true,
+    );
+  } catch (e) {
+    print("Error initializing Workmanager: $e");
+  }
+
+  try {
+    await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+  } catch (e) {
+    print("Error initializing Notifications: $e");
+  }
+
   runApp(const MyApp());
 }
 
@@ -19,14 +55,27 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => RestaurantProvider(apiService: ApiService()),
         ),
+        ChangeNotifierProvider(
+          create: (_) => DatabaseProvider(databaseHelper: DatabaseHelper()),
+        ),
+        ChangeNotifierProvider(create: (_) => SchedulingProvider()),
+        ChangeNotifierProvider(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+              sharedPreferences: SharedPreferences.getInstance(),
+            ),
+          ),
+        ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Restaurant App',
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: ThemeMode.system,
-        home: const RestaurantListPage(),
+      child: Consumer<PreferencesProvider>(
+        builder: (context, provider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Restaurant App',
+            theme: provider.themeData,
+            home: const RestaurantListPage(),
+          );
+        },
       ),
     );
   }
