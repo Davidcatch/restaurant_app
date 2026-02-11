c5e3109a4c41cbed9566371df09c4896bd7b6a9f
Raihan <raihanwibyanto@gmail.com>
2026-02-11 18:08:14 +0700

    update: styles

--- lib/common/styles.dart
+++ lib/common/styles.dart
@@ -1,81 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const Color kPrimaryColor = Color(0xFFE85858);
-const Color kSecondaryColor = Color(0xFFFFC08B);
-const Color kDarkBackground = Color(0xFF121212);
-const Color kLightBackground = Colors.white;
-
-final TextTheme myTextTheme = TextTheme(
-  displayLarge: GoogleFonts.poppins(
-    fontSize: 93,
-    fontWeight: FontWeight.w300,
-    letterSpacing: -1.5,
-  ),
-  displayMedium: GoogleFonts.poppins(
-    fontSize: 58,
-    fontWeight: FontWeight.w300,
-    letterSpacing: -0.5,
-  ),
-  displaySmall: GoogleFonts.poppins(fontSize: 46, fontWeight: FontWeight.w400),
-  headlineMedium: GoogleFonts.poppins(
-    fontSize: 33,
-    fontWeight: FontWeight.w400,
-    letterSpacing: 0.25,
-  ),
-  headlineSmall: GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w400),
-  titleLarge: GoogleFonts.poppins(
-    fontSize: 19,
-    fontWeight: FontWeight.w500,
-    letterSpacing: 0.15,
-  ),
-  titleMedium: GoogleFonts.poppins(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    letterSpacing: 0.15,
-  ),
-  titleSmall: GoogleFonts.poppins(
-    fontSize: 13,
-    fontWeight: FontWeight.w500,
-    letterSpacing: 0.1,
-  ),
-  bodyLarge: GoogleFonts.openSans(
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-    letterSpacing: 0.5,
-  ),
-  bodyMedium: GoogleFonts.openSans(
-    fontSize: 14,
-    fontWeight: FontWeight.w400,
-    letterSpacing: 0.25,
-  ),
-);
+final Color primaryColor = const Color(0xFFFFFFFF);
+final Color secondaryColor = const Color(0xFF6B38FB);
 
 ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primaryColor: kPrimaryColor,
-  scaffoldBackgroundColor: kLightBackground,
-  textTheme: myTextTheme,
-  appBarTheme: const AppBarTheme(
-    elevation: 0,
-    backgroundColor: kLightBackground,
+  colorScheme: ColorScheme.fromSeed(seedColor: secondaryColor),
+  useMaterial3: true,
+  textTheme: GoogleFonts.poppinsTextTheme(),
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.white,
     foregroundColor: Colors.black,
+    elevation: 0,
   ),
-  colorScheme: ColorScheme.fromSwatch().copyWith(secondary: kSecondaryColor),
 );
 
-ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primaryColor: kPrimaryColor,
-  scaffoldBackgroundColor: kDarkBackground,
-  textTheme: myTextTheme.apply(
-    bodyColor: Colors.white,
-    displayColor: Colors.white,
+ThemeData darkTheme = ThemeData.dark().copyWith(
+  colorScheme: ColorScheme.dark(
+    primary: secondaryColor,
+    onPrimary: Colors.black,
+    secondary: secondaryColor,
   ),
-  appBarTheme: const AppBarTheme(
-    elevation: 0,
-    backgroundColor: kDarkBackground,
-    foregroundColor: Colors.white,
-  ),
-  colorScheme: ColorScheme.dark().copyWith(secondary: kSecondaryColor),
+  textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
 );
